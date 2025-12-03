2567ec044311c7cd331d8d4883de30e0a14fbdcd
ferjeni ameni <ferjeniameni37@gmail.com>
2025-12-03 01:23:18 +0100

    commit 3

--- src/main/java/com/example/pipline_projet/PiplineProjetApplication.java
+++ src/main/java/com/example/pipline_projet/PiplineProjetApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PiplineProjetApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PiplineProjetApplication.class, args);
     }
